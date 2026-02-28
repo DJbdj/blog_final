01b82d2bd648644d3cc41983f1d6d16cd9447334
DJbdj <98082029+DJbdj@users.noreply.github.com>
2026-02-28 16:32:55 +0800

    112

--- src/features/theme/themes/magic/index.ts
+++ src/features/theme/themes/magic/index.ts
@@ -6,20 +6,6 @@ import { AuthLayout } from "./layouts/auth-layout";
 import { UserLayout } from "./layouts/user-layout";
 
 // Pages
-<<<<<<< HEAD
-import { HomePage, HomePageSkeleton } from "./pages/home/page";
-import { PostsPage, PostsPageSkeleton } from "./pages/posts/page";
-import { PostPage, PostPageSkeleton } from "./pages/post/page";
-import { FriendLinksPage, FriendLinksPageSkeleton } from "./pages/friend-links/page";
-import { SearchPage } from "./pages/search/page";
-import { SubmitFriendLinkPage } from "./pages/submit-friend-link/page";
-import { LoginPage } from "./pages/auth/login/page";
-import { RegisterPage } from "./pages/auth/register/page";
-import { ForgotPasswordPage } from "./pages/auth/forgot-password/page";
-import { ResetPasswordPage } from "./pages/auth/reset-password/page";
-import { VerifyEmailPage } from "./pages/auth/verify-email/page";
-import { ProfilePage } from "./pages/user/profile/page";
-=======
 import { HomePage, HomePageSkeleton } from "./pages/home";
 import { PostsPage, PostsPageSkeleton } from "./pages/posts";
 import { PostPage, PostPageSkeleton } from "./pages/post";
@@ -32,7 +18,6 @@ import { ForgotPasswordPage } from "./pages/auth/forgot-password";
 import { ResetPasswordPage } from "./pages/auth/reset-password";
 import { VerifyEmailPage } from "./pages/auth/verify-email";
 import { ProfilePage } from "./pages/user/profile";
->>>>>>> 93bfe12d60f5976d9dd4a72b0639dad9d7ff5861
 
 // Components
 import { config } from "./config";
@@ -60,4 +45,5 @@ export default {
   VerifyEmailPage,
   ProfilePage,
   Toaster,
+
 } satisfies ThemeComponents;
