b756d4a5cf5c1108d54cde282f411f48811dfafd
BDJarch <dj1482332494@126.com>
2026-03-14 11:43:28 +0800

    避免客户端路由问题

--- src/features/theme/themes/zlu/layouts/public-layout.tsx
+++ src/features/theme/themes/zlu/layouts/public-layout.tsx
@@ -48,12 +48,12 @@ export function PublicLayout({
             </div>
 
             <nav className="zlu-nav-menu">
-              <Link to="/" className="zlu-nav-item">
+              <a href="/" className="zlu-nav-item">
                 <svg style={{ width: '1.25rem', height: '1.25rem' }} fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1v2a1 1 0 001 1h1a1 1 0 001-1v-4a1 1 0 011-1v2a1 1 0 001 1h1m-6 0h6" />
                 </svg>
                 首页
-              </Link>
+              </a>
               <Link to="/posts" className="zlu-nav-item">
                 <svg style={{ width: '1.25rem', height: '1.25rem' }} fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-9-3h19" />
